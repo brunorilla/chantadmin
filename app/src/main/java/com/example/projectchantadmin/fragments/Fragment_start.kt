523dd226c803fec404d3ea0e73045ad39db1ef1c
FdValls <fernandodanielvalls@gmail.com>
2022-10-05 22:56:35 -0300

    Auth OK, dataBase OK

--- app/src/main/java/com/example/projectchantadmin/fragments/Fragment_start.kt
+++ app/src/main/java/com/example/projectchantadmin/fragments/Fragment_start.kt
@@ -1,19 +1,32 @@
 package com.example.projectchantadmin.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.projectchantadmin.R
+import com.example.projectchantadmin.activities.SplashActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.HashMap
 
 class Fragment_start : Fragment() {
 
-    private lateinit var buttonStartLogin : Button
-    private lateinit var buttonCreateUser : Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var idButtonLogout: Button
+    private lateinit var idButtonDB: Button
     private lateinit var viewStart: View
+    private lateinit var applicationContext: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +39,10 @@ class Fragment_start : Fragment() {
         // Inflate the layout for this fragment
         viewStart = inflater.inflate(R.layout.fragment_start, container, false)
 
-        buttonStartLogin = viewStart.findViewById(R.id.id_start_inicioSesion)
-        buttonCreateUser = viewStart.findViewById(R.id.id_start_createUser)
-
+        idButtonLogout = viewStart.findViewById(R.id.id_button_logout)
+        idButtonDB = viewStart.findViewById(R.id.id_button_dataBase)
+        applicationContext = findNavController().context
+        auth = Firebase.auth
 
         return viewStart
     }
@@ -36,8 +50,16 @@ class Fragment_start : Fragment() {
     override fun onStart() {
         super.onStart()
 
-        buttonStartLogin.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_fragment_start_to_fragment_login, null))
-        buttonCreateUser.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_fragment_start_to_fragment_main, null))
+        idButtonLogout.setOnClickListener {
+            val windowStart = Intent(applicationContext, SplashActivity::class.java)
+            auth.signOut()
+            startActivity(windowStart)
+        }
+
+        idButtonDB.setOnClickListener {
+            Toast.makeText(applicationContext, "Generando datos....", Toast.LENGTH_SHORT)
+                .show()
+        }
 
     }
 
